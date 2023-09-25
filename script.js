75ce0c4313f02a1c820af874934e6bd2f9288f0d
Felipe Bassi <fkb5105@psu.edu>
2023-09-25 10:59:33 -0400

    extra features

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-function Menubar() {
-    var x = document.getElementById("myMenu");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
+document.addEventListener("DOMContentLoaded", function () {
+    function Menubar() {
+        var x = document.getElementById("myMenu");
+        if (x.className === "menu") {
+            x.className += " responsive";
+        } else {
+            x.className = "menu";
+        }
     }
-  }
-  
 
-    // Smooth scroll to anchor links
-    $('a[href^="#"]').on('click', function(e) {
+    let mybutton = document.getElementById("myBtn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // Add this line to attach topFunction to the button's click event
+    mybutton.addEventListener("click", topFunction);
+
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    $('a[href^="#"]').on("click", function (e) {
         e.preventDefault();
         var target = $(this.hash);
         if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 500);
-            toggleMobileMenu(); // Close the mobile menu when a link is clicked
+            $("html, body").animate(
+                {
+                    scrollTop: target.offset().top,
+                },
+                500
+            );
+            Menubar(); // Close the mobile menu when a link is clicked
         }
     });
-
-    // Show or hide the sticky footer button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            $('.go-top').fadeIn(200);
-        } else {
-            $('.go-top').fadeOut(200);
-        }
-    });
-
-    // Animate the scroll to top
-    $('.go-top').click(function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 300);
-    });
-
-    
+});
