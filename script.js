8d5aa2cb10c8555c632e5414d98ba9549144227d
Felipe Bassi <fkb5105@psu.edu>
2023-10-04 16:38:47 -0400

    back to top button

--- script.js
+++ script.js
@@ -35,6 +35,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 15);
     });
 
+    let mybutton = document.getElementById("myBtn");
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    mybutton.addEventListener("click", topFunction);
+
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
     $('a[href^="#"]').on("click", function (e) {
         e.preventDefault();
         var target = $(this.hash);
@@ -49,3 +72,4 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
